bbf766fb4c9d18522610c7fe77af18ac30b99230
JGeun <pjk8926@naver.com>
2023-03-15 09:54:18 +0900

    refact: rename dao parameters

--- core/database/src/main/java/jgeun/study/mypokemon/database/dao/PokemonDao.kt
+++ core/database/src/main/java/jgeun/study/mypokemon/database/dao/PokemonDao.kt
@@ -13,8 +13,8 @@ interface PokemonDao {
     suspend fun insertPokemonList(pokemonList: List<PokemonEntity>)
 
     @Query("SELECT * FROM PokemonEntity WHERE page = :page_")
-    suspend fun getPokemonList(page_: Int): List<PokemonEntity>
+    suspend fun getPokemonList(page: Int): List<PokemonEntity>
 
     @Query("SELECT * FROM PokemonEntity WHERE page <= :page_")
-    suspend fun getAllPokemonList(page_: Int): List<PokemonEntity>
+    suspend fun getAllPokemonList(page: Int): List<PokemonEntity>
 }
