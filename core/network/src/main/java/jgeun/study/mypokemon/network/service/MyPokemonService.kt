b604a6c48be6b6e83f9750f5f8dd078889c34846
JGeun <pjk8926@naver.com>
2023-03-15 09:52:43 +0900

    refact: change service function name (get -> fetch)

--- core/network/src/main/java/jgeun/study/mypokemon/network/service/MyPokemonService.kt
+++ core/network/src/main/java/jgeun/study/mypokemon/network/service/MyPokemonService.kt
@@ -1,9 +1,10 @@
 package jgeun.study.mypokemon.network.service
 
+import com.skydoves.sandwich.ApiResponse
 import jgeun.study.mypokemon.model.Pokemon
+import jgeun.study.mypokemon.model.PokemonInfo
 import jgeun.study.mypokemon.model.PokemonType
 import jgeun.study.mypokemon.network.model.BasePokemonResponse
-import jgeun.study.mypokemon.network.model.PokemonDetailResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -11,16 +12,18 @@ import retrofit2.http.Query
 interface MyPokemonService {
 
     @GET("pokemon")
-    suspend fun getPokemonList(
+    suspend fun fetchPokemonList(
         @Query("limit") limit: Int = 20,
         @Query("offset") offset: Int = 0
-    ): BasePokemonResponse<Pokemon>
+    ): ApiResponse<BasePokemonResponse<Pokemon>>
 
-    @GET("pokemon/{id}")
-    suspend fun getPokemon(
-        @Path("id") id: Int
-    ): PokemonDetailResponse
+    @GET("pokemon/{name}")
+    suspend fun fetchPokemonInfo(
+        @Path("name") name: String
+    ): ApiResponse<PokemonInfo>
 
     @GET("type")
-    suspend fun getPokemonType() : BasePokemonResponse<PokemonType>
+    suspend fun fetchPokemonTypeList(
+
+    ) : ApiResponse<BasePokemonResponse<List<PokemonType>>>
 }
